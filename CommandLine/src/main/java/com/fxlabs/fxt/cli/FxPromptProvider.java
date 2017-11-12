9776220fe7f5a4f38aee6fe831a62ea19e208572
Intesar Mohammed <mdshannan@gmail.com>
2017-11-12 14:46:43 -0800

    CLI prompt customizations

---
+++ CommandLine/src/main/java/com/fxlabs/fxt/cli/FxPromptProvider.java
@@ -0,0 +1,18 @@
+package com.fxlabs.fxt.cli;
+
+
+import org.jline.utils.AttributedString;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.shell.jline.PromptProvider;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class FxPromptProvider implements PromptProvider {
+
+    public AttributedString getPrompt() {
+        return new AttributedString("Fx:> ");
+    }
+}
+
