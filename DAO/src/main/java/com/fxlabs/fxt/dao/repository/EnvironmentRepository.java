510406134132495612dd6996f7ded4ad24c7b638
Intesar Mohammed <mdshannan@gmail.com>
2017-11-25 11:50:23 -0800

    ProjectEnv refactored to Env

--- DAO/src/main/java/com/fxlabs/fxt/dao/repository/EnvironmentRepository.java
+++ DAO/src/main/java/com/fxlabs/fxt/dao/repository/EnvironmentRepository.java
@@ -3,5 +3,5 @@ package com.fxlabs.fxt.dao.repository;
 import com.fxlabs.fxt.dao.entity.project.Environment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProjectEnvironmentRepository extends JpaRepository<Environment, String> {
+public interface EnvironmentRepository extends JpaRepository<Environment, String> {
 }
--- Services/src/main/java/com/fxlabs/fxt/services/project/EnvironmentServiceImpl.java
+++ Services/src/main/java/com/fxlabs/fxt/services/project/EnvironmentServiceImpl.java
@@ -2,7 +2,7 @@ package com.fxlabs.fxt.services.project;
 
 import com.fxlabs.fxt.converters.project.EnvironmentConverter;
 import com.fxlabs.fxt.dao.entity.project.Environment;
-import com.fxlabs.fxt.dao.repository.ProjectEnvironmentRepository;
+import com.fxlabs.fxt.dao.repository.EnvironmentRepository;
 import com.fxlabs.fxt.services.base.GenericServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,7 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class EnvironmentServiceImpl extends GenericServiceImpl<Environment, com.fxlabs.fxt.dto.project.Environment, String> implements EnvironmentService {
 
     @Autowired
-    public EnvironmentServiceImpl(ProjectEnvironmentRepository repository, EnvironmentConverter converter) {
+    public EnvironmentServiceImpl(EnvironmentRepository repository, EnvironmentConverter converter) {
         super(repository, converter);
     }
 
