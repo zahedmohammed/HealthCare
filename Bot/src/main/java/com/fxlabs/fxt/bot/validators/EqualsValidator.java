ac1a28d5fee793b36d0f9d8133a553b0a471a409
Intesar Mohammed <mdshannan@gmail.com>
2018-01-24 11:37:22 -0800

    Added EMPTY support in equals/not-equals comparator

--- Bot/src/main/java/com/fxlabs/fxt/bot/validators/EqualsValidator.java
+++ Bot/src/main/java/com/fxlabs/fxt/bot/validators/EqualsValidator.java
@@ -9,7 +9,9 @@ public class EqualsValidator extends Validator {
 
     @Override
     public void validate(String operand1, String operand2, Context context, String assertion) {
-        if (StringUtils.equals(operand1, operand2)) {
+        if (StringUtils.equalsIgnoreCase(operand2, "EMPTY") && StringUtils.isEmpty(operand1)) {
+            context.setResult("pass");
+        } else if (StringUtils.equals(operand1, operand2)) {
             context.setResult("pass");
         } else {
             validationFailed(operand1, operand2, context, assertion);
--- Bot/src/main/java/com/fxlabs/fxt/bot/validators/NotEqualsValidator.java
+++ Bot/src/main/java/com/fxlabs/fxt/bot/validators/NotEqualsValidator.java
@@ -13,7 +13,9 @@ public class NotEqualsValidator extends Validator {
 
     @Override
     public void validate(String operand1, String operand2, Context context, String assertion) {
-        if (!StringUtils.equals(operand1, operand2)) {
+        if (StringUtils.equalsIgnoreCase(operand2, "EMPTY") && StringUtils.isNotEmpty(operand1)) {
+            context.setResult("pass");
+        } else if (!StringUtils.equals(operand1, operand2)) {
             context.setResult("pass");
         } else {
             validationFailed(operand1, operand2, context, assertion);
