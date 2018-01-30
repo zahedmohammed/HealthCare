370e486d425409d7e3afc6da49ca11fde582654b
Intesar Mohammed <mdshannan@gmail.com>
2018-01-29 21:23:50 -0800

    index.html working

--- Web/src/main/java/com/fxlabs/fxt/SecurityConfiguration.java
+++ Web/src/main/java/com/fxlabs/fxt/SecurityConfiguration.java
@@ -9,9 +9,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebSecurity
+//@EnableWebMvc
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
