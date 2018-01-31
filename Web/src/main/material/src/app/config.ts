549187fd594b3f8b40868b15617d4093e4ed9c52
Intesar Mohammed <mdshannan@gmail.com>
2018-01-31 14:52:27 -0800

    ui

--- Web/src/main/material/src/app/config.ts
+++ Web/src/main/material/src/app/config.ts
@@ -4,18 +4,18 @@ function makeAppConfig() {
   const year = date.getFullYear();
 
   const AppConfig = {
-    brand: 'Material',
+    brand: 'Fx Labs, Inc',
     user: 'Lisa',
     year,
     layoutBoxed: false,               // true, false
-    navCollapsed: false,              // true, false
+    navCollapsed: true,              // true, false
     navBehind: false,                 // true, false
-    fixedHeader: true,                // true, false
-    sidebarWidth: 'middle',           // small, middle, large
+    fixedHeader: false,                // true, false
+    sidebarWidth: 'small',           // small, middle, large
     theme: 'light',                   // light, gray, dark
-    colorOption: '34',                // 11,12,13,14,15,16; 21,22,23,24,25,26; 31,32,33,34,35,36
+    colorOption: '36',                // 11,12,13,14,15,16; 21,22,23,24,25,26; 31,32,33,34,35,36
     AutoCloseMobileNav: true,         // true, false. Automatically close sidenav on route change (Mobile only)
-    productLink: 'https://themeforest.net/item/material-design-angular-2-admin-web-app-with-bootstrap-4/19421267'
+    productLink: 'https://fxlabs.io/'
   };
 
   return AppConfig;
