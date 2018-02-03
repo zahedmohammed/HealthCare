5b490507fc531831a261ea0d4374e2c68796f2fd
Intesar Mohammed <mdshannan@gmail.com>
2018-02-02 18:35:30 -0800

    Added BaaS, NaaS, GaaS initial work

---
+++ GaaS/src/main/java/com/fxlabs/fxt/gaas/amqp/Receiver.java
@@ -0,0 +1,26 @@
+package com.fxlabs.fxt.gaas.amqp;
+
+
+import com.fxlabs.fxt.dto.run.BotTask;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Intesar Shannan Mohammed
+ */
+@Component
+public class Receiver {
+
+    final Logger logger = LoggerFactory.getLogger(getClass());
+
+    //@Autowired
+    //RestProcessor restProcessor;
+
+    public void receiveMessage(BotTask task) {
+        logger.info("Task id [{}] name [{}]", task.getId(), task.getSuiteName());
+        //restProcessor.process(task);
+    }
+
+}
---
+++ NaaS/src/main/java/com/fxlabs/fxt/naas/amqp/Application.java
@@ -0,0 +1,45 @@
+package com.fxlabs.fxt.naas.amqp;
+
+import org.springframework.amqp.core.AcknowledgeMode;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author Intesar Shannan Mohammed
+ */
+@SpringBootApplication(scanBasePackages = {"com.fxlabs.fxt.bot"})
+public class Application {
+
+    public static void main(String[] args) throws InterruptedException {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+                                             MessageListenerAdapter listenerAdapter,
+                                             @Value("${fx.naas.queue}") String queueName,
+                                             @Value("${concurrentConsumers}") int concurrentConsumers,
+                                             @Value("${maxConcurrentConsumers}") int maxConcurrentConsumers) {
+
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueueNames(queueName);
+        container.setConcurrentConsumers(concurrentConsumers);
+        container.setMaxConcurrentConsumers(maxConcurrentConsumers);
+        container.setDefaultRequeueRejected(false);
+        container.setAcknowledgeMode(AcknowledgeMode.AUTO);
+        container.setMessageListener(listenerAdapter);
+        return container;
+    }
+
+    @Bean
+    MessageListenerAdapter listenerAdapter(Receiver receiver) {
+        return new MessageListenerAdapter(receiver, "receiveMessage");
+    }
+
+}
