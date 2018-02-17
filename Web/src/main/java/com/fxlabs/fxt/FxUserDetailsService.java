8688676bf1291fab94a38be7bbdfcea67421f829
Intesar Mohammed <mdshannan@gmail.com>
2018-02-17 11:12:01 -0800

    removed UserService from UserDetailsService

--- Web/src/main/java/com/fxlabs/fxt/FxUserDetailsService.java
+++ Web/src/main/java/com/fxlabs/fxt/FxUserDetailsService.java
@@ -1,16 +1,19 @@
 package com.fxlabs.fxt;
 
 import com.fxlabs.fxt.converters.users.OrgConverter;
+import com.fxlabs.fxt.converters.users.UsersConverter;
+import com.fxlabs.fxt.converters.users.UsersPasswordConverter;
 import com.fxlabs.fxt.dao.entity.users.Org;
 import com.fxlabs.fxt.dao.entity.users.OrgRole;
 import com.fxlabs.fxt.dao.entity.users.OrgUserStatus;
 import com.fxlabs.fxt.dao.entity.users.OrgUsers;
 import com.fxlabs.fxt.dao.repository.jpa.OrgUsersRepository;
+import com.fxlabs.fxt.dao.repository.jpa.UsersPasswordRepository;
+import com.fxlabs.fxt.dao.repository.jpa.UsersRepository;
 import com.fxlabs.fxt.dto.base.Response;
 import com.fxlabs.fxt.dto.users.Users;
 import com.fxlabs.fxt.dto.users.UsersPassword;
 import com.fxlabs.fxt.rest.base.FxUserPrinciple;
-import com.fxlabs.fxt.services.users.UsersService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -27,7 +30,16 @@ import java.util.Optional;
 public class FxUserDetailsService implements UserDetailsService {
 
     @Autowired
-    private UsersService usersService;
+    private UsersRepository usersRepository;
+
+    @Autowired
+    private UsersConverter usersConverter;
+
+    @Autowired
+    private UsersPasswordRepository usersPasswordRepository;
+
+    @Autowired
+    private UsersPasswordConverter usersPasswordConverter;
 
     @Autowired
     private OrgUsersRepository orgUsersRepository;
@@ -50,23 +62,27 @@ public class FxUserDetailsService implements UserDetailsService {
             orgName = tokens[0];
             email = tokens[1];
         }
-        final Response<Users> usersResponse = usersService.findByEmail(username);
-        if (usersResponse.isErrors()) {
-            throw new UsernameNotFoundException(username);
+        Optional<com.fxlabs.fxt.dao.entity.users.Users> usersOptional = usersRepository.findByEmail(email);
+        if (!usersOptional.isPresent()) {
+            throw new UsernameNotFoundException(email);
         }
-        final Response<UsersPassword> usersPasswordResponse = usersService.findActivePassword(username);
-        if (usersPasswordResponse.isErrors()) {
-            throw new UsernameNotFoundException(usersPasswordResponse.getMessages().get(0).getValue());
+
+        Optional<com.fxlabs.fxt.dao.entity.users.UsersPassword> usersPasswordOptional = this.usersPasswordRepository.findByUsersEmailAndActive(email, true);
+        if (!usersPasswordOptional.isPresent()) {
+            throw new UsernameNotFoundException(email);
         }
+
+        com.fxlabs.fxt.dao.entity.users.Users users = usersOptional.get();
+        com.fxlabs.fxt.dao.entity.users.UsersPassword usersPassword = usersPasswordOptional.get();
 
         com.fxlabs.fxt.dto.users.Org org = null;
         if (StringUtils.isNotEmpty(orgName)) {
-            Optional<OrgUsers> orgOptional = orgUsersRepository.findByUsersIdAndStatusAndOrgNameAndOrgRole(usersResponse.getData().getId(), OrgUserStatus.ACTIVE, orgName, OrgRole.ADMIN);
+            Optional<OrgUsers> orgOptional = orgUsersRepository.findByUsersIdAndStatusAndOrgNameAndOrgRole(users.getId(), OrgUserStatus.ACTIVE, orgName, OrgRole.ADMIN);
             if (orgOptional.isPresent()) {
                 org = orgConverter.convertToDto(orgOptional.get().getOrg());
             }
         }
 
-        return new FxUserPrinciple(usersResponse.getData(), usersPasswordResponse.getData(), org);
+        return new FxUserPrinciple(usersConverter.convertToDto(usersOptional.get()), usersPasswordConverter.convertToDto(usersPassword), org);
     }
 }
