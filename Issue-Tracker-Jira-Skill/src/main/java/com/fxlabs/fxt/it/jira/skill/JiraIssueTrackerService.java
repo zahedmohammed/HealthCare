3a5d92bc9db6ab49bc6cc8d6f0e8cb86b281236e
intesar <mdshannan@gmail.com>
2018-03-07 23:18:32 +0530

    JIRA issue tracker - issue create, update - initial code

--- Issue-Tracker-Jira-Skill/src/main/java/com/fxlabs/fxt/it/jira/skill/JiraIssueTrackerService.java
+++ Issue-Tracker-Jira-Skill/src/main/java/com/fxlabs/fxt/it/jira/skill/JiraIssueTrackerService.java
@@ -1,11 +1,23 @@
 package com.fxlabs.fxt.it.jira.skill;
 
+import com.atlassian.jira.rest.client.api.IssueRestClient;
+import com.atlassian.jira.rest.client.api.JiraRestClient;
+import com.atlassian.jira.rest.client.api.JiraRestClientFactory;
+import com.atlassian.jira.rest.client.api.domain.BasicIssue;
+import com.atlassian.jira.rest.client.api.domain.input.IssueInput;
+import com.atlassian.jira.rest.client.api.domain.input.IssueInputBuilder;
+import com.atlassian.jira.rest.client.internal.async.AsynchronousJiraRestClientFactory;
+import com.atlassian.util.concurrent.Promise;
 import com.fxlabs.fxt.dto.it.ITTaskResponse;
+import com.fxlabs.fxt.dto.run.TestCaseResponse;
 import com.fxlabs.fxt.it.skill.services.IssueTrackerService;
 import com.fxlabs.fxt.it.skill.services.ITTask;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Component
 public class JiraIssueTrackerService implements IssueTrackerService {
@@ -43,13 +55,32 @@ public class JiraIssueTrackerService implements IssueTrackerService {
      *  </p>
      */
     @Override
-    public ITTaskResponse process(final ITTask task) {
-
+    public ITTaskResponse process(final TestCaseResponse task) {
         ITTaskResponse response = new ITTaskResponse();
-
         try {
-           // TODO Create/Update bugs/ussue in issue tracker
+           // TODO Create/Update bugs/ussue in issue tracker JIRA
 
+            String url = task.getEndpoint();
+            URI uri = new URI(url);
+
+            final String JIRA_USERNAME = ""; //get from task
+            final String JIRA_PASSWORD = ""; // get from task
+
+            JiraRestClientFactory factory = new AsynchronousJiraRestClientFactory();
+            JiraRestClient client = factory.createWithBasicHttpAuthentication(uri, JIRA_USERNAME, JIRA_PASSWORD);
+
+            if (task.getResult().equals("FAIL")) {
+                // TODO: if it is a test rerun result and there is no issue created already for this test case
+                BasicIssue issue = createIssue(client, task);
+                response.setIssueId(issue.getKey());
+            }else{
+                // TODO: if there was already an issue, and the TC passed now, update the issue
+                // Find the corresponding issue
+//                updateIssue(client, task, issue);
+            }
+            response.setSuccess(true);
+            response.setLogs(taskLogger.get().toString());
+            return response;
         } catch (RuntimeException ex) {
             logger.warn(ex.getLocalizedMessage(), ex);
             //response.setLogs(taskLogger.get().toString());
@@ -59,6 +90,68 @@ public class JiraIssueTrackerService implements IssueTrackerService {
         }
 
         return response;
+
+    }
+
+//    public JiraRestClient init(){
+//
+//        final String JIRA_URL = "https://luqmans.atlassian.net";
+//        final String JIRA_ADMIN_USERNAME = "luqmanshareef@gmail.com";
+//        final String JIRA_ADMIN_PASSWORD = "luqssh123";
+//        JiraRestClient client = null;
+//
+//        URI uri;
+//        try {
+//            JiraRestClientFactory factory = new AsynchronousJiraRestClientFactory();
+//            uri = new URI(JIRA_URL);
+//            client = factory.createWithBasicHttpAuthentication(uri, JIRA_ADMIN_USERNAME, JIRA_ADMIN_PASSWORD);
+//        } catch (URISyntaxException e) {
+//            e.printStackTrace();
+//            return null;
+//        }
+//        return client;
+//    }
+
+    private BasicIssue createIssue(JiraRestClient client, TestCaseResponse task){
+        IssueRestClient issueClient = client.getIssueClient();
+
+        String projectKey = "LFP"; // get from task
+        StringBuffer summary = new StringBuffer();
+        summary.append(task.getResponse()  );
+        summary.append(task.getResult()  );
+
+        StringBuffer desc = new StringBuffer();
+        desc.append(task.getTestCase());
+        desc.append(task.getResult());
+
+        IssueInput newIssue = new IssueInputBuilder(projectKey, 10004L, summary.toString())
+//				.setAssigneeName("admin")
+                .setPriorityId(5L)
+//				.setReporterName("admin")
+                .setDescription(desc.toString())
+                .build();
+        Promise<BasicIssue> basicIssue = issueClient.createIssue(newIssue);
+        System.out.println(basicIssue.claim().getKey() + " created.");
+        return basicIssue.claim();
+    }
+
+    private BasicIssue updateIssue(JiraRestClient client, TestCaseResponse task, String issueKey){
+
+        IssueRestClient issueClient = client.getIssueClient();
+        BasicIssue issue = issueClient.getIssue(issueKey).claim(); // get Key from task
+
+        String projectKey = "";  //get from task
+
+        IssueInput issueInput = new IssueInputBuilder(projectKey, 10004L)
+//				.setAssigneeName("admin")
+                .setPriorityId(5L)
+//				.setReporterName("admin")
+//                .setDescription(desc.toString())
+                .build();
+
+        issueClient.updateIssue(issueKey, issueInput).claim();
+
+        return issue;
 
     }
 
