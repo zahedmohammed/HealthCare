6adebbf094f2c73907492bba5432f53621139b62
Mohammed Shoukath Ali <shoukathmd@gmail.com>
2018-03-13 09:49:01 +0530

    GCP Intial Commit

---
+++ Cloud-GCP-Skill/src/main/java/com/fxlabs/fxt/cloud/gcp/skill/GCPCloudService.java
@@ -0,0 +1,114 @@
+package com.fxlabs.fxt.cloud.gcp.skill;
+
+import com.fxlabs.fxt.cloud.skill.services.CloudService;
+import com.fxlabs.fxt.dto.cloud.CloudTask;
+import com.fxlabs.fxt.dto.cloud.CloudTaskResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+
+/**
+ *
+ * @author Mohammed Shoukath Ali
+ *
+ *
+ */
+
+@Component
+public class GCPCloudService implements CloudService {
+
+    final Logger logger = LoggerFactory.getLogger(getClass());
+    public ThreadLocal<StringBuilder> taskLogger = new ThreadLocal<>();
+
+    /**
+     * <p>
+     *  This method does only one thing.
+     *   1. Creates Vm's on GCP
+     * </p>
+     *
+     * @param task
+     *  <p>
+     *      Contains public cloud system connection information.
+     *      e.g.
+     *     VM configuaration
+     *  </p>
+     *
+     *
+     * @return
+     *  <p>
+     *      CloudTaskResponse - Should only set these properties.
+     *      1. success - true/false
+     *      2. logs - execution or error logs.
+     *  </p>
+     */
+    @Override
+    public CloudTaskResponse create(final CloudTask task) {
+        logger.info("In IT AwsCloud Service for task [{}]" , task.getType().toString());
+
+        CloudTaskResponse response = new CloudTaskResponse();
+
+        try {
+
+
+            return response;
+
+        } catch (RuntimeException ex) {
+            logger.warn(ex.getLocalizedMessage(), ex);
+            response.setLogs(taskLogger.get().toString());
+        } catch (Exception ex) {
+            logger.warn(ex.getLocalizedMessage(), ex);
+            taskLogger.get().append(ex.getLocalizedMessage()).append("\n");
+        }
+
+        return response;
+
+    }
+
+    /**
+     * <p>
+     *  This method does only one thing.
+     *   1. Destroys Vm's on GCP
+     * </p>
+     *
+     * @param task
+     *  <p>
+     *      Contains public cloud system connection information.
+     *      e.g.
+     *     VM configuaration
+     *  </p>
+     *
+     *
+     * @return
+     *  <p>
+     *      CloudTaskResponse - Should only set these properties.
+     *      1. success - true/false
+     *      2. logs - execution or error logs.
+     *  </p>
+     */
+
+
+    @Override
+    public CloudTaskResponse destroy(final CloudTask task) {
+        logger.info("In IT AwsCloud Service for task [{}]" , task.getType().toString());
+
+        CloudTaskResponse response = new CloudTaskResponse();
+
+        try {
+
+
+            return response;
+
+        } catch (RuntimeException ex) {
+            logger.warn(ex.getLocalizedMessage(), ex);
+            response.setLogs(taskLogger.get().toString());
+        } catch (Exception ex) {
+            logger.warn(ex.getLocalizedMessage(), ex);
+            taskLogger.get().append(ex.getLocalizedMessage()).append("\n");
+        }
+
+        return response;
+
+    }
+
+}
---
+++ Cloud-GCP-Skill/src/test/java/com/fxlabs/fxt/cloud/gcp/skill/services/ServiceTest.java
@@ -0,0 +1,23 @@
+package com.fxlabs.fxt.cloud.gcp.skill.services;
+
+import com.fxlabs.fxt.cloud.skill.services.CloudService;
+import com.fxlabs.fxt.dto.it.ITTask;
+import org.junit.Test;
+
+//@RunWith(SpringJUnit4ClassRunner.class)
+public class ServiceTest {
+
+    //@Autowired
+    private CloudService service;
+
+    @Test
+    public void process() throws Exception {
+
+        CloudTask task = new CloudTask();
+//        task.setProjectId("fx-100");
+//        task.setVcUrl("https://github.com/intesar/HTML.git");
+        //System.out.println(service.process(task));
+
+    }
+
+}
