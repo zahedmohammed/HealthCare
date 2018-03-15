cfeafe8e117e33e921f63b0db083a9c7624513b4
Mohammed Shoukath Ali <shoukathmd@gmail.com>
2018-03-15 18:32:33 +0530

    Droped Unused Columns

---
+++ DAO/src/main/resources/db/migration/V22__testcaseresponse_alter_column.sql
@@ -0,0 +1,8 @@
+--
+-- Name: test_case_response Drop columns
+-- author Mohammed Shoukath Ali
+--
+
+ALTER TABLE ONLY test_case_response DROP COLUMN IF EXISTS name;
+ALTER TABLE ONLY test_case_response DROP COLUMN IF EXISTS _key;
+ALTER TABLE ONLY test_case_response DROP COLUMN IF EXISTS itKey;
