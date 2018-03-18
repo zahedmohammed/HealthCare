5b59f2eba6961857150ce9ec3ec761f2cbe69196
Intesar Mohammed <mdshannan@gmail.com>
2018-03-18 00:31:04 -0700

    test_case_response table fixes.

--- DAO/src/main/resources/db/migration/V26__test_case_response_skill_type.sql
+++ DAO/src/main/resources/db/migration/V26__test_case_response_skill_type.sql
@@ -7,5 +7,20 @@ ALTER TABLE ONLY test_case_response
     DROP COLUMN IF EXISTS skill_type;
 
 ALTER TABLE ONLY test_case_response
+    DROP COLUMN IF EXISTS testcase;
+
+ALTER TABLE ONLY test_case_response
     ALTER COLUMN test_case DROP NOT NULL;
+
+ALTER TABLE ONLY test_case_response
+    DROP COLUMN IF EXISTS endpointeval;
+
+ALTER TABLE ONLY test_case_response
+    ALTER COLUMN endpoint_eval TYPE text;
+
+ALTER TABLE ONLY test_case_response
+    DROP COLUMN IF EXISTS requesteval;
+
+ALTER TABLE ONLY test_case_response
+    ALTER COLUMN request_eval TYPE text;
 
