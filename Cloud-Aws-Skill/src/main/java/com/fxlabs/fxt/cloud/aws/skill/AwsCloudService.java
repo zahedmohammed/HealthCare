694e6ff467b036c36994395cd91f9fc9bb90a13b
Mohammed Shoukath Ali <shoukathmd@gmail.com>
2018-03-20 16:03:24 +0530

    Aws Intial cut

--- Cloud-Aws-Skill/src/main/java/com/fxlabs/fxt/cloud/aws/skill/AwsCloudService.java
+++ Cloud-Aws-Skill/src/main/java/com/fxlabs/fxt/cloud/aws/skill/AwsCloudService.java
@@ -3,9 +3,31 @@ package com.fxlabs.fxt.cloud.aws.skill;
 import com.fxlabs.fxt.cloud.skill.services.CloudService;
 import com.fxlabs.fxt.dto.cloud.CloudTask;
 import com.fxlabs.fxt.dto.cloud.CloudTaskResponse;
+import com.google.common.collect.ImmutableSet;
+import com.google.inject.Module;
+import org.jclouds.ContextBuilder;
+import org.jclouds.compute.ComputeService;
+import org.jclouds.compute.ComputeServiceContext;
+import org.jclouds.compute.RunNodesException;
+import org.jclouds.compute.domain.OsFamily;
+import org.jclouds.compute.domain.Template;
+import org.jclouds.compute.domain.TemplateBuilder;
+import org.jclouds.ec2.domain.InstanceType;
+import org.jclouds.enterprise.config.EnterpriseConfigurationModule;
+import org.jclouds.logging.slf4j.config.SLF4JLoggingModule;
+import org.jclouds.sshj.config.SshjSshClientModule;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import static org.jclouds.compute.config.ComputeServiceProperties.TIMEOUT_SCRIPT_COMPLETE;
+import static org.jclouds.ec2.domain.InstanceType.T2_MICRO;
 
 
 /**
@@ -75,6 +97,39 @@ public class AwsCloudService implements CloudService {
         try {
 
 
+            if(CollectionUtils.isEmpty(task.getOpts())){
+                // TODO Add appro msgs and send response
+                return response;
+            }
+
+            Map<String, String> opts = task.getOpts();
+
+            // Args
+            String accessKeyId = opts.get("ACCESS_KEY_ID");
+            String secretKey = opts.get("SECRET_KEY");
+
+
+            String image = task.getOpts().get("IMAGE");
+            String subscription = task.getOpts().get("SUBSCRIPTION");
+
+            String hardware = task.getOpts().get("HARDWARE");
+            String locationId = task.getOpts().get("LOCATION");
+
+
+            ComputeService awsService = getAwsService(accessKeyId, secretKey);
+
+            TemplateBuilder templateBuilder = awsService.templateBuilder();
+            taskLogger.get().append("AwsCloudService: hardware id " + InstanceType.T2_MEDIUM);
+            templateBuilder.hardwareId(T2_MICRO);
+
+
+
+            taskLogger.get().append("AwsCloudService: image id " + image);
+
+            templateBuilder.osFamily(OsFamily.UBUNTU).osVersionMatches("16.04").imageDescriptionMatches("ubuntu-images");
+            Template template = templateBuilder.build();
+            awsService.createNodesInGroup("fxlabs", 1, template);
+
             return response;
 
         } catch (RuntimeException ex) {
@@ -88,5 +143,29 @@ public class AwsCloudService implements CloudService {
         return response;
 
     }
+
+    private static ComputeService getAwsService(String accessKeyId, String secretKey) {
+
+        Properties properties = new Properties();
+
+        long scriptTimeout = TimeUnit.MILLISECONDS.convert(20, TimeUnit.MINUTES);
+        properties.setProperty(TIMEOUT_SCRIPT_COMPLETE, scriptTimeout + "");
+
+
+        Iterable<Module> modules = ImmutableSet.<Module>of(
+                new SshjSshClientModule(),
+                new SLF4JLoggingModule(),
+                new EnterpriseConfigurationModule());
+
+        ContextBuilder builder = ContextBuilder.newBuilder("aws-ec2")
+                .credentials(accessKeyId, secretKey)
+                .modules(modules)
+                .overrides(properties);
+        System.out.print(builder.getApiMetadata());
+        ComputeService client = builder.buildView(ComputeServiceContext.class).getComputeService();
+
+        return client;
+    }
+
 
 }
