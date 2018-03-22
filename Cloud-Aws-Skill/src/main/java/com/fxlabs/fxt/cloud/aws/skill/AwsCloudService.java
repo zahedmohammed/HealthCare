1d3d9af23dbd752549c28e1678c2754d62df0a6d
Mohammed Shoukath Ali <shoukathmd@gmail.com>
2018-03-22 21:55:26 +0530

    Aws bot deployment.

--- Cloud-Aws-Skill/src/main/java/com/fxlabs/fxt/cloud/aws/skill/AwsCloudService.java
+++ Cloud-Aws-Skill/src/main/java/com/fxlabs/fxt/cloud/aws/skill/AwsCloudService.java
@@ -9,9 +9,12 @@ import org.jclouds.ContextBuilder;
 import org.jclouds.compute.ComputeService;
 import org.jclouds.compute.ComputeServiceContext;
 import org.jclouds.compute.RunNodesException;
+import org.jclouds.compute.domain.NodeMetadata;
 import org.jclouds.compute.domain.OsFamily;
 import org.jclouds.compute.domain.Template;
 import org.jclouds.compute.domain.TemplateBuilder;
+import org.jclouds.compute.options.TemplateOptions;
+import org.jclouds.domain.LoginCredentials;
 import org.jclouds.ec2.domain.InstanceType;
 import org.jclouds.enterprise.config.EnterpriseConfigurationModule;
 import org.jclouds.logging.slf4j.config.SLF4JLoggingModule;
@@ -20,12 +23,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
+import static com.google.common.collect.Iterables.getOnlyElement;
 import static org.jclouds.compute.config.ComputeServiceProperties.TIMEOUT_SCRIPT_COMPLETE;
 import static org.jclouds.ec2.domain.InstanceType.T2_MICRO;
 
@@ -41,6 +46,8 @@ import static org.jclouds.ec2.domain.InstanceType.T2_MICRO;
 public class AwsCloudService implements CloudService {
 
     final Logger logger = LoggerFactory.getLogger(getClass());
+
+    private static final String AWS_PKEY = "";
     public ThreadLocal<StringBuilder> taskLogger = new ThreadLocal<>();
 
     /**
@@ -69,7 +76,7 @@ public class AwsCloudService implements CloudService {
         logger.info("In IT AwsCloud Service for task [{}]", task.getType().toString());
 
         CloudTaskResponse response = new CloudTaskResponse();
-
+        response.setSuccess(false);
         try {
 
             if (CollectionUtils.isEmpty(task.getOpts())) {
@@ -94,18 +101,50 @@ public class AwsCloudService implements CloudService {
             ComputeService awsService = getAwsService(accessKeyId, secretKey);
 
             TemplateBuilder templateBuilder = awsService.templateBuilder();
-            taskLogger.get().append("AwsCloudService: hardware id " + InstanceType.T2_MEDIUM);
-            templateBuilder.hardwareId(T2_MICRO);
 
+            //Instance type/size
+            if(StringUtils.isEmpty(hardware)){
+                hardware = InstanceType.T2_MICRO.toString();
+            }
+            taskLogger.get().append("setting hardware id " + hardware);
+            templateBuilder.hardwareId(hardware);
 
-            taskLogger.get().append("AwsCloudService: image id " + image);
+            //image
+            if(StringUtils.isEmpty(image)){
+                image = "us-west-1/ami-09d2fb69";
+            }
+            taskLogger.get().append("Setting image id : " + image);
+            templateBuilder.imageId(image);
 
-            templateBuilder.osFamily(OsFamily.UBUNTU).osVersionMatches("16.04").imageDescriptionMatches("ubuntu-images");
             Template template = templateBuilder.build();
-            awsService.createNodesInGroup("fxlabs", 1, template);
+
+            TemplateOptions options = template.getOptions();
+
+            String username = getAwsImageUsername(opts);
+            String password = getPassword(opts);
+
+            if(StringUtils.isEmpty(password)){
+                password = AWS_PKEY;
+            }
+            if (!skipBotInstallation(opts)) {
+                taskLogger.get().append("Installing Bot.....");
+                LoginCredentials login = null;
+
+                if (!StringUtils.isEmpty(username)) {
+                    login = LoginCredentials.builder().user(username).privateKey(password).build();
+                } else {
+                    login = LoginCredentials.builder().privateKey(password).build();
+                }
+
+                if (login != null) {
+                    options.runAsRoot(true).runScript(getBotInstallationScript(opts)).overrideLoginCredentials(login);
+                }
+            }
+            NodeMetadata virtualMachine = getOnlyElement(awsService.createNodesInGroup("fxlabs", 1, template));
+            response.setSuccess(true);
+            response.setId(virtualMachine.getId());
 
             return response;
-
         } catch (RuntimeException ex) {
             logger.warn(ex.getLocalizedMessage(), ex);
             response.setLogs(taskLogger.get().toString());
@@ -164,5 +203,44 @@ public class AwsCloudService implements CloudService {
         return client;
     }
 
+    private Boolean skipBotInstallation(Map<String, String> map) {
+
+        String value = map.get("skip_bot_install");
+
+        if (!StringUtils.isEmpty(value)
+                && org.apache.commons.lang3.StringUtils.equalsIgnoreCase(value, "true")) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private String getAwsImageUsername(Map<String, String> opts) {
+        String value = opts.get("image_username");
+        if (org.apache.commons.lang3.StringUtils.equalsIgnoreCase(value, "null")) {
+            return "";
+        }
+        return value;
+    }
+
+    private String getPassword(Map<String, String> opts){
+        String password = opts.get("vm_password");
+        if (org.apache.commons.lang3.StringUtils.equalsIgnoreCase(password, "null")) {
+            return "";
+        }
+        if(StringUtils.isEmpty(password)){
+            return password;
+        }
+
+        return password;
+    }
+
+    private String getBotInstallationScript(Map<String, String> opts) {
+        String value = opts.get("SCRIPT");
+        if (org.apache.commons.lang3.StringUtils.equalsIgnoreCase(value, "null")) {
+            return "";
+        }
+        return value;
+    }
 
 }
