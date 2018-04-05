f75989fdee5d99b479f0ff897aaf6b75e12ab974
Intesar Mohammed <mdshannan@gmail.com>
2018-04-04 23:29:43 -0700

    Invalid Cred Generator

--- Codegen/src/main/java/com/fxlabs/fxt/codegen/generators/InvalidCredGenerator.java
+++ Codegen/src/main/java/com/fxlabs/fxt/codegen/generators/InvalidCredGenerator.java
@@ -1,13 +1,28 @@
 package com.fxlabs.fxt.codegen.generators;
 
 import com.fxlabs.fxt.dto.project.TestSuiteMin;
+import com.fxlabs.fxt.dto.project.TestSuiteType;
 import io.swagger.models.Operation;
 import org.springframework.stereotype.Component;
 
 @Component(value = "invalidCredGenerator")
-public class InvalidCredGenerator implements Generator {
+public class InvalidCredGenerator extends AbstractGenerator {
+
+    protected static final String POSTFIX = "auth_invalid";
+    protected static final String AUTH = "INVALID_AUTH";
+    protected static final String OPERAND = "401";
 
     public TestSuiteMin generate(String path, io.swagger.models.HttpMethod method, Operation op) {
-        return null;
+        TestSuiteMin testSuite = build(op, path, POSTFIX, op.getDescription(), TestSuiteType.SUITE, method, TAG, AUTH);
+
+        // TODO - if Security required
+        buildAssertion(testSuite, STATUS_CODE_ASSERTION, EQUALS, OPERAND);
+
+        // TODO buildTestCase(testSuite)
+        if (method == io.swagger.models.HttpMethod.POST || method == io.swagger.models.HttpMethod.PUT) {
+
+        }
+
+        return testSuite;
     }
 }
