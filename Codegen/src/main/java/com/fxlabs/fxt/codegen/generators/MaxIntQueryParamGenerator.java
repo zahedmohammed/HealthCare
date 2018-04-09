eaa0b63bca4a2ecd5c37e34f00031a2ce1cd18ce
intesar <mdshannan@gmail.com>
2018-04-09 17:42:46 +0530

    Generarte negative Test suites for Query params

---
+++ Codegen/src/main/java/com/fxlabs/fxt/codegen/generators/MaxIntQueryParamGenerator.java
@@ -0,0 +1,48 @@
+package com.fxlabs.fxt.codegen.generators;
+
+import com.fxlabs.fxt.codegen.generators.base.AbstractGenerator;
+import com.fxlabs.fxt.dto.project.TestSuiteMin;
+import com.fxlabs.fxt.dto.project.TestSuiteType;
+import io.swagger.models.Operation;
+import io.swagger.models.parameters.Parameter;
+import io.swagger.models.parameters.QueryParameter;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Mohammed Luqman Shareef
+ * @since 4/9/2018
+ */
+@Component(value = "queryParamGeneratorMaxInt")
+public class MaxIntQueryParamGenerator extends AbstractGenerator {
+
+    protected static final String POSTFIX = "query_param_max_int";
+    protected static final String AUTH = "BASIC";
+    protected static final String OPERAND = "200";
+
+    @Override
+    public List<TestSuiteMin> generate(String path, io.swagger.models.HttpMethod method, Operation op) {
+
+        List<TestSuiteMin> allTestSuites = new ArrayList<>();
+        if (method == io.swagger.models.HttpMethod.GET) {
+            for ( Parameter param : op.getParameters()){
+                if (param instanceof QueryParameter){
+                    QueryParameter queryParam = (QueryParameter) param;
+                    if ("integer".equals(queryParam.getType())){
+                        String postFix = POSTFIX + "_" + queryParam.getName() ;
+                        List<TestSuiteMin> testSuites = build(op, path, postFix, op.getDescription(), TestSuiteType.SUITE, method, TAG, AUTH);
+                        for (TestSuiteMin testSuite : testSuites) {
+                            buildAssertion(testSuite, STATUS_CODE_ASSERTION, NOT_EQUALS, OPERAND);
+                            testSuite.setEndpoint(path + "?" + queryParam.getName() + "=" + Integer.MAX_VALUE);
+                        }
+                        allTestSuites.addAll(testSuites);
+                    }
+                }
+            }
+        }
+        return allTestSuites;
+
+    }
+}
---
+++ Codegen/src/main/java/com/fxlabs/fxt/codegen/generators/NegativeQueryParamGenerator.java
@@ -0,0 +1,49 @@
+package com.fxlabs.fxt.codegen.generators;
+
+import com.fxlabs.fxt.codegen.generators.base.AbstractGenerator;
+import com.fxlabs.fxt.dto.project.TestCase;
+import com.fxlabs.fxt.dto.project.TestSuiteMin;
+import com.fxlabs.fxt.dto.project.TestSuiteType;
+import io.swagger.models.Operation;
+import io.swagger.models.parameters.QueryParameter;
+import org.springframework.stereotype.Component;
+import io.swagger.models.parameters.Parameter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Mohammed Luqman Shareef
+ * @since 4/9/2018
+ */
+@Component(value = "queryParamGeneratorNegative")
+public class NegativeQueryParamGenerator extends AbstractGenerator {
+
+    protected static final String POSTFIX = "query_param_negative";
+    protected static final String AUTH = "BASIC";
+    protected static final String OPERAND = "200";
+
+    @Override
+    public List<TestSuiteMin> generate(String path, io.swagger.models.HttpMethod method, Operation op) {
+
+        List<TestSuiteMin> allTestSuites = new ArrayList<>();
+        if (method == io.swagger.models.HttpMethod.GET) {
+            for ( Parameter param : op.getParameters()){
+                if (param instanceof QueryParameter){
+                    QueryParameter queryParam = (QueryParameter) param;
+                    if ("integer".equals(queryParam.getType())){
+                        String postFix = POSTFIX + "_" + queryParam.getName() ;
+                        List<TestSuiteMin> testSuites = build(op, path, postFix, op.getDescription(), TestSuiteType.SUITE, method, TAG, AUTH);
+                        for (TestSuiteMin testSuite : testSuites) {
+                            buildAssertion(testSuite, STATUS_CODE_ASSERTION, NOT_EQUALS, OPERAND);
+                            testSuite.setEndpoint(path + "?" + queryParam.getName() + "=" + "-1");
+                        }
+                        allTestSuites.addAll(testSuites);
+                    }
+                }
+            }
+        }
+        return allTestSuites;
+
+    }
+}
