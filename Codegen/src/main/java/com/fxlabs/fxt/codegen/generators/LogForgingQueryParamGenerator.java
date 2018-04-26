63447b78acf90493f25258f515e458b9749fcaa5
intesar <mdshannan@gmail.com>
2018-04-26 19:48:38 +0530

    Log Forging generator

---
+++ Codegen/src/main/java/com/fxlabs/fxt/codegen/generators/LogForgingQueryParamGenerator.java
@@ -0,0 +1,45 @@
+package com.fxlabs.fxt.codegen.generators;
+
+import com.fxlabs.fxt.codegen.generators.base.AbstractGenerator;
+import com.fxlabs.fxt.dto.project.TestSuiteMin;
+import com.fxlabs.fxt.dto.project.TestSuiteType;
+import io.swagger.models.Operation;
+import io.swagger.models.parameters.Parameter;
+import io.swagger.models.parameters.QueryParameter;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Mohammed Luqman Shareef
+ * @since 4/24/2018
+ */
+@Component(value = "logForgingQueryParamGenerator")
+public class LogForgingQueryParamGenerator extends AbstractGenerator {
+
+    protected static final String POSTFIX = "query_param_log_forging";
+    protected static final String AUTH = "BASIC";
+    protected static final String OPERAND = "200";
+
+    @Override
+    public List<TestSuiteMin> generate(String path, io.swagger.models.HttpMethod method, Operation op) {
+
+        List<TestSuiteMin> allTestSuites = new ArrayList<>();
+        if (method == io.swagger.models.HttpMethod.GET) {
+            for (Parameter param : op.getParameters()) {
+                if (param instanceof QueryParameter) {
+                    QueryParameter queryParam = (QueryParameter) param;
+                    String postFix = POSTFIX + "_" + queryParam.getName();
+                    List<TestSuiteMin> testSuites = build(op, path, postFix, op.getDescription(), TestSuiteType.SUITE, method, TAG, AUTH);
+                    for (TestSuiteMin testSuite : testSuites) {
+                        buildAssertion(testSuite, STATUS_CODE_ASSERTION, NOT_EQUALS, OPERAND);
+                        testSuite.setEndpoint(path + "?" + queryParam.getName() + "=" + "{{@LogForging | sample:all }}");
+                    }
+                    allTestSuites.addAll(testSuites);
+                }
+            }
+        }
+        return allTestSuites;
+    }
+}
