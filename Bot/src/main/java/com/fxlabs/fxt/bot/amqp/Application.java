76a72ce39afd04b1db64f79ef8b1de5edb47297c
Intesar Mohammed <mdshannan@gmail.com>
2018-04-26 12:43:02 -0700

    RabbitMQ SSL support added.

--- Bot/src/main/java/com/fxlabs/fxt/bot/amqp/Application.java
+++ Bot/src/main/java/com/fxlabs/fxt/bot/amqp/Application.java
@@ -1,13 +1,16 @@
 package com.fxlabs.fxt.bot.amqp;
 
 import org.springframework.amqp.core.AcknowledgeMode;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.connection.RabbitConnectionFactoryBean;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.amqp.RabbitProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -28,6 +31,7 @@ public class Application {
                                              @Value("${fx.default.queue}") String queueName,
                                              @Value("${concurrentConsumers}") int concurrentConsumers,
                                              @Value("${maxConcurrentConsumers}") int maxConcurrentConsumers) {
+
 
         SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
         container.setConnectionFactory(connectionFactory);
@@ -50,5 +54,68 @@ public class Application {
     MessageListenerAdapter listenerAdapter(Receiver receiver) {
         return new MessageListenerAdapter(receiver, "receiveMessage");
     }
+
+    @Bean
+    public CachingConnectionFactory rabbitConnectionFactory(RabbitProperties config)
+            throws Exception {
+
+        RabbitConnectionFactoryBean factory = new RabbitConnectionFactoryBean();
+
+        factory.setPort(config.determinePort());
+        factory.setHost(config.determineHost());
+
+        if (config.determineUsername() != null) {
+            factory.setUsername(config.determineUsername());
+        }
+        if (config.determinePassword() != null) {
+            factory.setPassword(config.determinePassword());
+        }
+        if (config.determineVirtualHost() != null) {
+            factory.setVirtualHost(config.determineVirtualHost());
+        }
+        if (config.getRequestedHeartbeat() != null) {
+            //factory.setRequestedHeartbeat(config.getRequestedHeartbeat().getSeconds());
+        }
+        RabbitProperties.Ssl ssl = config.getSsl();
+        if (ssl.isEnabled()) {
+            factory.setUseSSL(true);
+            if (ssl.getAlgorithm() != null) {
+                factory.setSslAlgorithm(ssl.getAlgorithm());
+            }
+            factory.setSkipServerCertificateValidation(true);
+            /*factory.setKeyStore(ssl.getKeyStore());
+            factory.setKeyStorePassphrase(ssl.getKeyStorePassword());
+            factory.setTrustStore(ssl.getTrustStore());
+            factory.setTrustStorePassphrase(ssl.getTrustStorePassword());*/
+        }
+        if (config.getConnectionTimeout() != null) {
+            //factory.setConnectionTimeout(config.getConnectionTimeout());
+        }
+        factory.afterPropertiesSet();
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory(
+                factory.getObject());
+        connectionFactory.setAddresses(config.determineAddresses());
+        connectionFactory.setPublisherConfirms(config.isPublisherConfirms());
+        connectionFactory.setPublisherReturns(config.isPublisherReturns());
+
+        if (config.getCache().getChannel().getSize() != null) {
+            connectionFactory
+                    .setChannelCacheSize(config.getCache().getChannel().getSize());
+        }
+        if (config.getCache().getConnection().getMode() != null) {
+            connectionFactory
+                    .setCacheMode(config.getCache().getConnection().getMode());
+        }
+        if (config.getCache().getConnection().getSize() != null) {
+            connectionFactory.setConnectionCacheSize(
+                    config.getCache().getConnection().getSize());
+        }
+        if (config.getCache().getChannel().getCheckoutTimeout() != null) {
+            //connectionFactory.setChannelCheckoutTimeout(config.getCache().getChannel().getCheckoutTimeout());
+        }
+        return connectionFactory;
+    }
+
+
 
 }
