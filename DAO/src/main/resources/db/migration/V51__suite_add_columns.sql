1e07aebf50aa79a253439501d240e793d2f61177
intesar <mdshannan@gmail.com>
2018-05-03 14:12:36 +0530

    SQL Script - Category and Severity added to suite

---
+++ DAO/src/main/resources/db/migration/V51__suite_add_columns.sql
@@ -0,0 +1,10 @@
+--
+-- Name: add columns to Suite
+-- Author: Mohammed Luqman Shareef
+--
+
+alter table suite add column category  character varying(255);
+
+alter table suite add column severity character varying(255);
+
+
