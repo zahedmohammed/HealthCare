9aa3f51ac35caf91e51c762e355f3718d5b67b1c
Mohammed Shoukath Ali <shoukathmd@gmail.com>
2018-05-04 16:50:16 +0530

    accounts fixes

--- DTO/src/main/java/com/fxlabs/fxt/dto/clusters/AccountType.java
+++ DTO/src/main/java/com/fxlabs/fxt/dto/clusters/AccountType.java
@@ -8,6 +8,6 @@ import java.util.List;
  */
 public enum AccountType {
 
-    AWS, BitBucket, GitHub, Git, GitLab, Microsoft_TFS_Git, Microsoft_VSTS_Git, SLACK, EMAIL, Local
+    AWS, BitBucket, GitHub, Git, GitLab, Microsoft_TFS_Git, Microsoft_VSTS_Git, Slack, Email, SLACK, EMAIL, Local
 
 }
