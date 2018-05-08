aec2518ae752524bbb627c402667c793b8b78ec1
Intesar Mohammed <mdshannan@gmail.com>
2018-05-07 22:35:32 -0700

    Fixes...

--- Services/src/main/java/com/fxlabs/fxt/services/project/JobServiceImpl.java
+++ Services/src/main/java/com/fxlabs/fxt/services/project/JobServiceImpl.java
@@ -15,10 +15,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
@@ -94,13 +91,15 @@ public class JobServiceImpl extends GenericServiceImpl<Job, com.fxlabs.fxt.dto.p
         // find owned projects org --> projects --> jobs
         // users --> org or users --> projects
         // least - a project should be visible to owner
+
+
         Response<List<Project>> projectsResponse = projectService.findProjects(user, pageable);
         if (projectsResponse.isErrors() || CollectionUtils.isEmpty(projectsResponse.getData())) {
-            return new Response<>().withMessages(projectsResponse.getMessages()).withErrors(true);
+            return new Response<List<com.fxlabs.fxt.dto.project.Job>>(Collections.emptyList(), 0L, 0);
         }
 
-        final List<Job> jobs = new ArrayList<>();
 
+        final List<Job> jobs = new ArrayList<>();
         projectsResponse.getData().stream().forEach(p -> {
             List<Job> _jobs = jobRepository.findByProjectIdAndInactive(p.getId(), false, PageRequest.of(0, 20));
             if (!CollectionUtils.isEmpty(_jobs)) {
