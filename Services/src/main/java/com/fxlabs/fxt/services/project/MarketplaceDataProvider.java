0ee36aeb67a7732db4782df7592be431330b59b0
Intesar Mohammed <mdshannan@gmail.com>
2018-06-18 19:40:17 -0700

    format fix.

--- Services/src/main/java/com/fxlabs/fxt/services/project/MarketplaceDataProvider.java
+++ Services/src/main/java/com/fxlabs/fxt/services/project/MarketplaceDataProvider.java
@@ -17,7 +17,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
