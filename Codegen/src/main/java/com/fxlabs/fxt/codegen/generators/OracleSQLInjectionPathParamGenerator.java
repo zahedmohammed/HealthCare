edd30b81c92a34beb0ece97de5588e460a828953
Mohammed Luqman Shareef <luqmanshareef@yahoo.com>
2018-07-02 13:09:44 +0530

    Injection Suites for Path Params

---
+++ Codegen/src/main/java/com/fxlabs/fxt/codegen/generators/OracleSQLInjectionPathParamGenerator.java
@@ -0,0 +1,74 @@
+package com.fxlabs.fxt.codegen.generators;
+
+import com.fxlabs.fxt.codegen.generators.base.AbstractGenerator;
+import com.fxlabs.fxt.dto.project.*;
+import io.swagger.models.Operation;
+import io.swagger.models.parameters.Parameter;
+import io.swagger.models.parameters.PathParameter;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Mohammed Luqman Shareef
+ * @since 4/24/2018
+ */
+@Component(value = "OracleSqlInjectionPathParamGenerator")
+public class OracleSQLInjectionPathParamGenerator extends AbstractGenerator {
+
+    protected static final String POSTFIX = "path_param_sql_injection_Oracle";
+    protected static final String AUTH = "Default";// BASIC
+    protected static final String OPERAND = "200";
+    protected static final String INJECTION_DATASET = "@OracleSQLInjections";
+    protected static final String DB_NAME = "Oracle";
+
+
+    @Override
+    public List<TestSuiteMin> generate(String path, io.swagger.models.HttpMethod method, Operation op) {
+
+        if (! isDB(DB_NAME)){
+            return null;
+        }
+        String dbVersion = getDBVersion(DB_NAME);
+
+
+        Policies policies =  new Policies();
+        policies.setRepeatModule(INJECTION_DATASET);
+
+        List<TestSuiteMin> allTestSuites = new ArrayList<>();
+        if (method == io.swagger.models.HttpMethod.GET) {
+            boolean first = true;
+            String paramSeparator = "?";
+
+            for (Parameter param : op.getParameters()) {
+//                if (param instanceof QueryParameter) {
+//                    QueryParameter queryParam = (QueryParameter) param;
+//                    String name = queryParam.getName();
+//                    String defaultVal = queryParam.getDefault() != null ? queryParam.getDefault().toString() : "{{@RandomInteger}}";
+//                    if (!first){
+//                        paramSeparator = "&";
+//                    }
+//                    path = path + paramSeparator + queryParam.getName() + "=" + "{{@RandomInteger}}";
+//                    first =false;
+//                }
+                if (param instanceof PathParameter) {
+                    PathParameter pathParam = (PathParameter) param;
+                    String postFix = POSTFIX + "_" + pathParam.getName();
+                    List<TestSuiteMin> testSuites = build(op, path, postFix, op.getDescription(), TestSuiteType.SUITE, method, TAG, AUTH, policies, false);
+                    for (TestSuiteMin testSuite : testSuites) {
+                        buildAssertion(testSuite, STATUS_CODE_ASSERTION, NOT_EQUALS, OPERAND);
+
+                        String _path = path.replace("{" + pathParam.getName() + "}", "{{"+INJECTION_DATASET+"}}");
+                        testSuite.setEndpoint(_path);
+
+                        testSuite.setCategory(TestSuiteCategory.Security_SQL_Injection);
+                        testSuite.setSeverity(TestSuiteSeverity.Major);
+                    }
+                    allTestSuites.addAll(testSuites);
+                }
+            }
+        }
+        return allTestSuites;
+    }
+}
