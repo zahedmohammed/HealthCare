f67942f6dde4ca1bfc4436adf9c421fe60d0ecbe
Mohammed Luqman Shareef <luqmanshareef@yahoo.com>
2018-07-04 11:59:45 +0530

    Writing Generated TS count in project

--- VC-Skill-Kit/src/main/java/com/fxlabs/fxt/vc/git/skill/services/VCDelegate.java
+++ VC-Skill-Kit/src/main/java/com/fxlabs/fxt/vc/git/skill/services/VCDelegate.java
@@ -66,7 +66,8 @@ public class VCDelegate {
                     try {
                         // 2/4. Auto-Code
                         CodegenThreadUtils.taskLogger.set(new com.fxlabs.fxt.codegen.code.BotLogger());
-                        stubGenerator.generate(task.getOpenAPISpec(), path + "/test-suites", path ,null, null);
+                        int count = stubGenerator.generate(task.getOpenAPISpec(), path + "/test-suites", path, null, null);
+                        response.setAutoGenSuitesCount(count);
                         // 3/4. Push to VC
                         gitPushLogs = versionControlService.push(path, task.getVcUsername(), task.getVcPassword());
                     } catch (Exception e) {
