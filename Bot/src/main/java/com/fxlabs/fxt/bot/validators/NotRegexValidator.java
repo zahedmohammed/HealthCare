b925686503401c60567d45bb6f6d4e8c9bd2b9ce
Intesar Mohammed <intesar>
2018-07-17 15:05:35 -0700

    NotRegex message fix.

--- Bot/src/main/java/com/fxlabs/fxt/bot/validators/NotRegexValidator.java
+++ Bot/src/main/java/com/fxlabs/fxt/bot/validators/NotRegexValidator.java
@@ -1,5 +1,6 @@
 package com.fxlabs.fxt.bot.validators;
 
+import com.fxlabs.fxt.bot.assertions.AssertionLogger;
 import com.fxlabs.fxt.bot.assertions.Context;
 import org.springframework.stereotype.Component;
 
@@ -16,5 +17,21 @@ public class NotRegexValidator extends Validator {
         } else {
             validationFailed(operand1, operand2, context, assertion, assertionLogs);
         }
+    }
+
+    protected void validationFailed(String operand1, String operand2, Context context, String assertion, StringBuilder assertionLogs) {
+        context.setResult("pass");
+        String msg = String.format("Assertion [%s] failed, not expecting [%s] but found [%s]", assertion, operand2, operand1);
+        //logger.debug(msg);
+        context.getLogs().append(AssertionLogger.LogType.INFO, context.getSuitename(), msg);
+        assertionLogs.append(msg);
+    }
+
+    protected void validationPass(String operand1, String operand2, Context context, String assertion, StringBuilder assertionLogs) {
+        context.setResult("pass");
+        String msg = String.format("Assertion [%s] passed, not expecting [%s] and found [%s]", assertion, operand2, operand1);
+        //logger.debug(msg);
+        context.getLogs().append(AssertionLogger.LogType.INFO, context.getSuitename(), msg);
+        assertionLogs.append(msg);
     }
 }
