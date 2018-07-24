b12c3643484cad3aaed5a937d79a15e8ec9b9e8d
muqtaar <mmuqtar008@gmail.com>
2018-07-24 16:02:30 +0530

    Autocodeconfig model

--- Web/src/main/ui/material/src/app/models/project-autocode-config.model.ts
+++ Web/src/main/ui/material/src/app/models/project-autocode-config.model.ts
@@ -1,9 +1,8 @@
 export class AutoCodeConfig {
     databases: Database[]=[];
     logForgingPatterns: String[]=[];
-    testSuites: String[]=[];
-
-
+    testSuites: TestSuite[]=[];
+    propertyMapping : Map<string, number>;
 }
 
 export class Database {
@@ -11,7 +10,48 @@ export class Database {
     public version: string;
     public description: string;
     public inactive: boolean;
+}   
+
+export class TestSuite {
+    project: ProjectMinimalDto = new ProjectMinimalDto();
+    public name: string;
+    public description:string;
+    public type: string;
+    public endpoint: string;
+    public method: string;
+    public auth: string;
+    headers: string[] = [];
+    testCases: TestCase[]= [];
+    assertions: string[]= [];
+    tags: string[]= [];
+    authors: string[] = [];
+    init: string[] = [];
+    cleanup: string[]= [];
+    policy: Policies = new Policies();
+    public publishToMarketplace: boolean;
+    props : Map<string, string> = new Map<string, string>();
+    public category: string;
+    public severity: string;
 }
 
+export  class  ProjectMinimalDto {
+    public name: string;
+    public org: string;
+}
 
+export class TestCase {
+    public id: number;
+    public body: string;
+    public inactive: boolean;
+}
 
+export class Policies {
+    public initExec: string;
+    public cleanupExec: string;
+    public logger: string;
+    public timeoutSeconds: number;
+    public repeatOnFailure: number;
+    public repeat: number;
+    public repeatModule: string;
+    public repeatDelay: number;
+}
