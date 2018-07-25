4882860ebf5b278a2544e333756b4850f402b3b6
muqtaar <mmuqtar008@gmail.com>
2018-07-25 16:07:49 +0530

    Re-modified model

--- Web/src/main/ui/material/src/app/models/project-autocode-config.model.ts
+++ Web/src/main/ui/material/src/app/models/project-autocode-config.model.ts
@@ -1,64 +1,25 @@
 export class AutoCodeConfig {
-    genPolicy: GenPolicy[] =[];
-    public openAPISpec: string;
-    databases: Database[]=[];
-    logForgingPatterns: String[]=[];
-    testSuites: TestSuite[]=[];
-    propertyMapping : Map<string, number>;
-}
 
-export class GenPolicy {
-    public None: string;
-    public Create: string;
-}
-
-export class Database {
-    public name: string;
-    public version: string;
-    public description: string;
-    public inactive: boolean;
-}
-
-export class TestSuite {
-    project: ProjectMinimalDto = new ProjectMinimalDto();
-    public name: string;
-    public description:string;
-    public type: string;
-    public endpoint: string;
-    public method: string;
-    public auth: string;
-    headers: string[] = [];
-    testCases: TestCase[]= [];
+    type: string;
     assertions: string[]= [];
-    tags: string[]= [];
-    authors: string[] = [];
-    init: string[] = [];
-    cleanup: string[]= [];
-    policy: Policies = new Policies();
-    public publishToMarketplace: boolean;
-    props : Map<string, string> = new Map<string, string>();
-    public category: string;
-    public severity: string;
+    severity: string;
+    inactive: boolean;
+    postfix: string;
+    match: Matches[] = [];
+    database: Databases[] = [];
 }
 
-export  class  ProjectMinimalDto {
-    public name: string;
-    public org: string;
+export class Matches{
+    name: string;
+    pathPatterns: string;
+    methods: string;
+    denyRoles: string;
+    sample: string;
+    queryParams: string;
+    bodyProperties: string;
 }
 
-export class TestCase {
-    public id: number;
-    public body: string;
-    public inactive: boolean;
-}
-
-export class Policies {
-    public initExec: string;
-    public cleanupExec: string;
-    public logger: string;
-    public timeoutSeconds: number;
-    public repeatOnFailure: number;
-    public repeat: number;
-    public repeatModule: string;
-    public repeatDelay: number;
+export class Databases{
+    name: string;
+    version: number;
 }
