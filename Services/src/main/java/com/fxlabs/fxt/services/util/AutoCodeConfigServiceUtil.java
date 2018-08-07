66ef50ae863fb774f6eaad74792d1b503dfa6265
Mohammed Shoukath Ali <shoukathmd@gmail.com>
2018-08-07 11:31:07 +0530

    sql timeboud changed  @ResponseTime < 7000

--- Services/src/main/java/com/fxlabs/fxt/services/util/AutoCodeConfigServiceUtil.java
+++ Services/src/main/java/com/fxlabs/fxt/services/util/AutoCodeConfigServiceUtil.java
@@ -244,7 +244,7 @@ public class AutoCodeConfigServiceUtil {
         sql_injection_timebound.setAssertionDescription("Successful test suite response codes are 200 or 408. UnSuccessful test suite response codes are 400, 404, 500.");
 
         List<String> assertions = new ArrayList<>();
-        assertions.add("@ResponseTime >= 5000");
+        assertions.add("@ResponseTime < 7000");
         assertions.add("@StatusCode != 401");
         assertions.add("@StatusCode != 404");
         assertions.add("@StatusCode != 500");
