3c2236d6d0276c019bf76b9f950319ff70b80e14
Samiullah <samiullah.net@gmail.com>
2018-08-28 15:58:18 +0530

    fix on org stepper

--- Web/src/main/ui/material/src/app/components/org/org-new/org-new.component.ts
+++ Web/src/main/ui/material/src/app/components/org/org-new/org-new.component.ts
@@ -3,7 +3,7 @@ import { Account } from './../../../models/account.model';
 import { Component, OnInit, Inject } from '@angular/core';
 import { Routes, RouterModule, Router, ActivatedRoute} from "@angular/router";
 import { OrgService } from '../../../services/org.service';
-import { Org } from '../../../models/org.model';
+import { Org, OrgUser, Member } from '../../../models/org.model';
 import { Handler } from '../../dialogs/handler/handler';
 import {VERSION, MatSnackBar, MatDialog, MatSnackBarConfig, MatDialogRef, MAT_DIALOG_DATA }from '@angular/material';
 import {SnackbarService}from '../../../services/snackbar.service';
@@ -24,7 +24,7 @@ export class OrgNewComponent implements OnInit {
   entry: Org = new Org();
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
-  account: Account = new Account();
+  account: Member = new Member();
   matStepper: MatStepper// MatStepper;@Input()
   isLinear = false;
 
@@ -35,16 +35,16 @@ export class OrgNewComponent implements OnInit {
   cloudShow: boolean = true;
   isValid: boolean = true;
 
-  cloudTypes = ['AWS','DIGITAL_OCEAN','GCP','AZURE','PRIVATE_CLOUD','VMWARE','OPENSTACK','OTHER'];
+  //cloudTypes = ['AWS','DIGITAL_OCEAN','GCP','AZURE','PRIVATE_CLOUD','VMWARE','OPENSTACK','OTHER'];
   //accountTypes = [ 'VERSION_CONTROL', 'ISSUE_TRACKER', 'CLOUD', 'NOTIFICATION'];
-  accountTypes = [
-      '--- Version Control ---', 'Git', 'GitHub', 'BitBucket', 'GitLab', 'Microsoft_TFS_Git', 'Microsoft_VSTS_Git', 'Local',
-      '--- Bot Deployment ---', 'AWS', 'Self_Hosted',
-      '--- Issue-Trackers ---', 'GitHub', 'Jira',
-      '--- Notifications ---', 'Slack' , 'Email'
-      ];
+  // accountTypes = [
+  //     '--- Version Control ---', 'Git', 'GitHub', 'BitBucket', 'GitLab', 'Microsoft_TFS_Git', 'Microsoft_VSTS_Git', 'Local',
+  //     '--- Bot Deployment ---', 'AWS', 'Self_Hosted',
+  //     '--- Issue-Trackers ---', 'GitHub', 'Jira',
+  //     '--- Notifications ---', 'Slack' , 'Email'
+  //     ];
 
-  AWSREGIONS = ['us-east-1','us-east-2','us-west-1','us-west-2','ca-central-1','eu-central-1','eu-west-1','eu-west-2','eu-west-3','ap-northeast-1','ap-northeast-2','ap-northeast-3','ap-southeast-1','ap-southeast-2','ap-southeast-1','sa-east-1'];
+  // AWSREGIONS = ['us-east-1','us-east-2','us-west-1','us-west-2','ca-central-1','eu-central-1','eu-west-1','eu-west-2','eu-west-3','ap-northeast-1','ap-northeast-2','ap-northeast-3','ap-southeast-1','ap-southeast-2','ap-southeast-1','sa-east-1'];
 
   constructor(private orgService: OrgService, private route: ActivatedRoute, private router: Router, private handler: Handler, private snackbarService: SnackbarService, private accountService: AccountService, private _formBuilder: FormBuilder) { }
 
@@ -78,21 +78,40 @@ export class OrgNewComponent implements OnInit {
     });
   }
 
+  // createAccount() {
+  //   this.handler.activateLoader();
+  //   this.snackbarService.openSnackBar(this.account.name + " creating...", "");
+  //   this.accountService.create(this.account).subscribe(results => {
+  //     this.handler.hideLoader();
+  //     if (this.handler.handle(results)) {
+  //       return;
+  //     }
+  //   this.snackbarService.openSnackBar(this.account.name + " created successfully", "");
+  //   this.router.navigate(['/app/orgs']);
+  //   }, error => {
+  //     this.handler.hideLoader();
+  //     this.handler.error(error);
+  //   });
+  // }
+
   createAccount() {
     this.handler.activateLoader();
     this.snackbarService.openSnackBar(this.account.name + " creating...", "");
-    this.accountService.create(this.account).subscribe(results => {
+    this.account.orgId = this.entry.id;
+    this.orgService.addMember(this.account.orgId, this.account).subscribe(results => {
       this.handler.hideLoader();
       if (this.handler.handle(results)) {
         return;
       }
-    this.snackbarService.openSnackBar(this.account.name + " created successfully", "");
-    this.router.navigate(['/app/orgs']);
+      this.snackbarService.openSnackBar(this.account.name + " created successfully", "");
+      this.router.navigate(['/app/orgs', this.entry.id, 'users']);
     }, error => {
       this.handler.hideLoader();
       this.handler.error(error);
     });
   }
+
+  roles = ['USER', 'PROJECT_MANAGER', 'ADMIN'];
 
 //   update() {
 //     this.handler.activateLoader();
@@ -120,13 +139,13 @@ export class OrgNewComponent implements OnInit {
 //     }
 //   }
 
-  toggleCloud() {
-    this.cloudShow = !this.cloudShow;
-  }
+  // toggleCloud() {
+  //   this.cloudShow = !this.cloudShow;
+  // }
 
-  visibilities = ['PRIVATE', 'ORG_PUBLIC'];
+  // visibilities = ['PRIVATE', 'ORG_PUBLIC'];
 
-    changeValue(valid: boolean) {
-        this.isValid = valid;
-    }
+  //   changeValue(valid: boolean) {
+  //       this.isValid = valid;
+  //   }
 }
