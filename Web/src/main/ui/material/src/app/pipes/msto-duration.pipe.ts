e663d04a7e86d7d438e8599333f7782a228c423c
muqtaar <mmuqtar008@gmail.com>
2018-09-06 16:54:01 +0530

    added milli seconds

--- Web/src/main/ui/material/src/app/pipes/msto-duration.pipe.ts
+++ Web/src/main/ui/material/src/app/pipes/msto-duration.pipe.ts
@@ -9,13 +9,16 @@ export class MstoDurationPipe implements PipeTransform {
     if(!value) value = 0;
     if(isNaN(value)) value = 0;
     let precision: number = 0;
+    let milli: number = value%1000;
+    value = value - milli;
     let seconds: number = (value/1000);
     let minutes: number = seconds/60;
     seconds = seconds%60;
     let hours: number = minutes/60;
     minutes = minutes%60;
+    
 
-    return hours.toFixed(precision) + ':' + minutes.toFixed(precision) + ':' + seconds.toFixed(precision);
+    return hours.toFixed(precision) + ':' + minutes.toFixed(precision) + ':' + seconds.toFixed(precision) + '.' + milli.toFixed(precision);
   }
 
 }
