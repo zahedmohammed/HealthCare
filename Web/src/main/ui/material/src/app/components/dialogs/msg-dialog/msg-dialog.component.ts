38b5443beb18e50df5431a1b12041a386f3b7b35
Muqeed <muqeed11@gmail.com>
2018-09-10 16:00:54 +0530

    Highlighting error on ace-editor Wire log

--- Web/src/main/ui/material/src/app/components/dialogs/msg-dialog/msg-dialog.component.ts
+++ Web/src/main/ui/material/src/app/components/dialogs/msg-dialog/msg-dialog.component.ts
@@ -1,19 +1,47 @@
-import { Component, Inject, OnInit } from "@angular/core";
+import {AfterViewInit, Component, Inject, OnInit, ViewChild} from "@angular/core";
 import { MatDialogRef,MAT_DIALOG_DATA } from '@angular/material';
+import 'brace';
+import 'brace/mode/yaml';
+import 'brace/theme/github';
+import 'brace/ext/language_tools';
+import { AceEditorComponent } from 'ng2-ace-editor';
 
 @Component({
-  selector: 'app-msg-dialog',
-  templateUrl: './msg-dialog.component.html',
-  styleUrls: ['./msg-dialog.component.scss']
+    selector: 'app-msg-dialog',
+    templateUrl: './msg-dialog.component.html',
+    styleUrls: ['./msg-dialog.component.scss']
 })
-export class MsgDialogComponent implements OnInit {
+export class MsgDialogComponent implements AfterViewInit {
 
-  constructor(
-    @Inject(MAT_DIALOG_DATA) public data: any,
-    public dialogRef: MatDialogRef<MsgDialogComponent>
-  ) { }
+    @ViewChild('editor') editor:AceEditorComponent
 
-  ngOnInit() {
-  }
+    constructor(@Inject(MAT_DIALOG_DATA) public data: any,
+                public dialogRef: MatDialogRef<MsgDialogComponent>) {
+
+    }
+
+    ngAfterViewInit() {
+
+        var array2:any = []
+
+        var numberoflines = this.editor.getEditor().session.getLength()
+
+        var words = this.editor.getEditor().session.getLines(0,numberoflines)
+
+        for(var i=0;i<words.length;i++) {
+            var array1  = words[i].split(" ")
+            for(var j=0;j<array1.length;j++)
+                if(array1[j] == "ERROR")
+                {
+                    j = array1.length
+                    array2.push({row:i,text:"Error",type:"error"})
+
+                }
+        }
+
+        this.editor.getEditor().getSession().setAnnotations(array2)
+
+    }
+
 
 }
