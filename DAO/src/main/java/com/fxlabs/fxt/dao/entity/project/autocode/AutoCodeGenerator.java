4193f1907388668b0a568586d21280888bb2dbac
jaleel <jaleel4mail@gmail.com>
2018-09-12 17:49:17 +0530

    Revert "Removed description columns from autoCode generator"
    This reverts commit 2ad0eabe805e7aeeb44493acc0166b62899601d9.

--- DAO/src/main/java/com/fxlabs/fxt/dao/entity/project/autocode/AutoCodeGenerator.java
+++ DAO/src/main/java/com/fxlabs/fxt/dao/entity/project/autocode/AutoCodeGenerator.java
@@ -28,13 +28,13 @@ public class AutoCodeGenerator extends BasicBaseEntity {
     @ElementCollection
     private List<String> assertions;
 
-    /*private String displayHeaderDescription;
+    private String displayHeaderDescription;
     private String displayHeaderLabel;
     private String assertionDescription;
 
     @Enumerated(EnumType.STRING)
     private TestSuiteSeverity severity;
-*/
+
     @Embedded
     private Database database;
 
