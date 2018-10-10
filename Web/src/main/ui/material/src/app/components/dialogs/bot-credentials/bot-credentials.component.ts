c48fcb27680cba5587e3869df55a6a5cfe9d3129
muqtaar <mmuqtar008@gmail.com>
2018-10-10 17:59:19 +0530

    create credential for azure

--- Web/src/main/ui/material/src/app/components/dialogs/bot-credentials/bot-credentials.component.ts
+++ Web/src/main/ui/material/src/app/components/dialogs/bot-credentials/bot-credentials.component.ts
@@ -17,9 +17,11 @@ import {Handler}from '../../dialogs/handler/handler';
 export class BotCredentialsComponent implements OnInit {
 
   entry: Account = new Account();
-  accountTypes = ['AWS', 'Self_Hosted'];
+  accountTypes = ['AWS', 'AZURE', 'Self_Hosted'];
   AWSREGIONS = ['us-east-1','us-east-2','us-west-1','us-west-2','ca-central-1','eu-central-1','eu-west-1','eu-west-2','eu-west-3','ap-northeast-1','ap-northeast-2','ap-northeast-3','ap-southeast-1','ap-southeast-2','ap-southeast-1','sa-east-1'];
-
+  AZUREREGIONS = ['West US', 'West US 2', 'Central US', 'East US', 'East US 2', 'North Central US', 'South Central US','West Central US',
+  'Canada Central', 'Canada East', 'Brazil South', 'North Europe', 'West Europe', 'UK South', 'UK West','East Asia', 'South East Asia', 'Japan East', 'Japan West', 'Australia East', 'Australia Southeast', 'Central India', 'South India',
+  'West India', 'Korea Central', 'Korea South','China North', 'China East','Germany Central', 'Germany Northeast'];     
   isValid: boolean = true;
   cloudShow: boolean = true;
 
