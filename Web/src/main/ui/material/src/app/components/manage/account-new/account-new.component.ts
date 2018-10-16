9ebdfb93a7bd81d0e2198aa4249c0262f434aea6
Atifa Fahmeen <atifa.fahmeen@gmail.com>
2018-10-16 09:34:07 +0530

    Grouping of Account Types While Creating new Credentials

--- Web/src/main/ui/material/src/app/components/manage/account-new/account-new.component.ts
+++ Web/src/main/ui/material/src/app/components/manage/account-new/account-new.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { Routes, RouterModule, Router, ActivatedRoute } from "@angular/router";
 import { AccountService } from '../../../services/account.service';
 import { OrgService } from '../../../services/org.service';
@@ -6,6 +7,17 @@ import { Account } from '../../../models/account.model';
 import { Handler } from '../../dialogs/handler/handler';
 import { VERSION, MatDialog, MatDialogRef } from '@angular/material';
 import { SnackbarService } from '../../../services/snackbar.service';
+
+export interface AccountType{
+value: string;
+viewValue: string;
+}
+
+export interface AccountTypeGroup{
+disabled?: boolean;
+name: string;
+accountType: AccountType[];
+}
 
 @Component({
   selector: 'app-account-new',
@@ -22,11 +34,43 @@ export class AccountNewComponent implements OnInit {
   isValid: boolean = true;
   cloudTypes = ['AWS', 'DIGITAL_OCEAN', 'GCP', 'AZURE', 'PRIVATE_CLOUD', 'VMWARE', 'OPENSTACK', 'OTHER'];
   //accountTypes = [ 'VERSION_CONTROL', 'ISSUE_TRACKER', 'CLOUD', 'NOTIFICATION'];
-  accountTypes = [
-    '--- Version Control ---', 'Git', 'GitHub', 'BitBucket', 'GitLab', 'Microsoft_TFS_Git', 'Microsoft_VSTS_Git', 'Local',
-    '--- Bot Deployment ---', 'AWS', 'AZURE', 'Self_Hosted',
-    '--- Issue-Trackers ---', 'FX_Issues', 'GitHub', 'Jira',
-    '--- Notifications ---', 'Slack', 'Email'
+
+  accountTypeGroup: AccountTypeGroup[]= [
+  {
+      name: 'Version Control',
+      accountType: [
+        {value: 'Git', viewValue: 'Git'},
+        {value: 'GitHub', viewValue: 'GitHub'},
+        {value: 'BitBucket', viewValue: 'BitBucket'},
+        {value: 'GitLab', viewValue: 'GitLab'},
+        {value: 'Microsoft_TFS_Git', viewValue: 'Microsoft_TFS_Git'},
+        {value: 'Microsoft_VSTS_Git', viewValue: 'Microsoft_VSTS_Git'},
+        {value: 'Local', viewValue: 'Local'}
+      ]
+    },
+    {
+      name: 'Bot Deployment',
+      accountType: [
+        {value: 'AWS', viewValue: 'AWS'},
+        {value: 'AZURE', viewValue: 'AZURE'},
+        {value: 'Self_Hosted', viewValue: 'Self_Hosted'}
+      ]
+    },
+    {
+      name: 'Issue Trackers',
+      accountType: [
+        {value: 'FX_Issues', viewValue: 'FX_Issues'},
+        {value: 'GitHub', viewValue: 'GitHub'},
+        {value: 'Jira', viewValue: 'Jira'}
+      ]
+    },
+    {
+      name: 'Notifications',
+      accountType: [
+        {value: 'Slack', viewValue: 'Slack'},
+        {value: 'Email', viewValue: 'Email'},
+      ]
+    }
   ];
 
   AWSREGIONS = ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2', 'ca-central-1', 'eu-central-1', 'eu-west-1', 'eu-west-2', 'eu-west-3', 'ap-northeast-1', 'ap-northeast-2', 'ap-northeast-3', 'ap-southeast-1', 'ap-southeast-2', 'ap-southeast-1', 'sa-east-1'];
