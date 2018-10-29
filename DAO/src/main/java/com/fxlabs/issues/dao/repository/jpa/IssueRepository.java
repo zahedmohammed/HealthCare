b39c00a1f5af698fab12e22ce0469ce3557c6b5d
Mohammed S Ali <shoukathmd@gmail.com>
2018-10-29 17:11:00 +0530

    remove unused imports

--- DAO/src/main/java/com/fxlabs/issues/dao/repository/jpa/IssueRepository.java
+++ DAO/src/main/java/com/fxlabs/issues/dao/repository/jpa/IssueRepository.java
@@ -1,14 +1,11 @@
 package com.fxlabs.issues.dao.repository.jpa;
 
 import com.fxlabs.issues.dao.entity.project.Issue;
-import com.fxlabs.issues.dao.entity.project.Project;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Stream;
 
 /**
  * @author Mohammed Shoukath Ali
