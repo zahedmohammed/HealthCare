fa6515191af19ddfaa5b20735dcc8d9ea2faed97
Atifa Fahmeen <atifa.fahmeen@gmail.com>
2018-11-29 13:48:30 +0530

    Adding Fake data to appointment and recepient

--- Services/src/main/java/com/fxlabs/issues/services/account/AppointmentServiceImpl.java
+++ Services/src/main/java/com/fxlabs/issues/services/account/AppointmentServiceImpl.java
@@ -1,16 +1,22 @@
 package com.fxlabs.issues.services.account;
 
 import com.fxlabs.issues.converters.account.AppointmentConverter;
+import com.fxlabs.issues.dao.entity.users.Users;
 import com.fxlabs.issues.dto.account.Appointment;
 import com.fxlabs.issues.dao.repository.jpa.AppointmentRepository;
 import com.fxlabs.issues.dto.base.Message;
 import com.fxlabs.issues.dto.base.MessageType;
 import com.fxlabs.issues.dto.base.Response;
 import com.fxlabs.issues.services.base.GenericServiceImpl;
+import com.github.javafaker.Faker;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -41,8 +47,55 @@ public class AppointmentServiceImpl extends GenericServiceImpl<com.fxlabs.issues
 
     @Override
     public Response<List<Appointment>> findAllAppointments(String currentAuditor) {
-        List<com.fxlabs.issues.dao.entity.account.Appointment> appointmentList = appointmentRepository.findAll();
+        List<com.fxlabs.issues.dao.entity.account.Appointment> appointmentList = new ArrayList<>();
+        Faker faker = new Faker();
+        String size = String.valueOf(faker.random().nextInt(1, 20));
+        for (int i = 0; i < 10; i++){
+            appointmentList.add(i, data());
+            System.out.println("Check Data ----" + appointmentList.toString());
+        }
+
         return new Response<List<Appointment>>(appointmentConverter.convertToDtos(appointmentList));
+    }
+
+    private com.fxlabs.issues.dao.entity.account.Appointment data() {
+
+        com.fxlabs.issues.dao.entity.account.Appointment appointmentData = new com.fxlabs.issues.dao.entity.account.Appointment();
+        Faker faker = new Faker();
+        Date date = Date.from(LocalDate.now().atStartOfDay(ZoneId.systemDefault()).toInstant());
+        String location = String.valueOf(faker.random());
+        String description = String.valueOf(faker.random());
+        boolean confirmed = Boolean.valueOf(faker.bool().bool()) ;
+
+        appointmentData.setDate(date);
+        appointmentData.setLocation(location);
+        appointmentData.setDescription(description);
+        appointmentData.setConfirmed(confirmed);
+        appointmentData.setCreatedDate(faker.date().birthday());
+        appointmentData.setModifiedDate(faker.date().birthday());
+        appointmentData.setId(faker.idNumber().valid());
+        appointmentData.setCreatedBy(faker.numerify("abcdefghijklmnopqrstuvwxyz123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+        appointmentData.setModifiedBy(faker.numerify("abcdefghijklmnopqrstuvwxyz123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+
+        Users user = new Users();
+        user.setAccountNonExpired(faker.bool().bool());
+        user.setAccountNonLocked(faker.bool().bool());
+        user.setAccountNumber(faker.number().numberBetween(1,10));
+        user.setCompany(faker.company().industry());
+        user.setEmail(faker.internet().emailAddress());
+        user.setLocation(faker.address().fullAddress());
+        user.setEnabled(faker.bool().bool());
+        user.setJobTitle(faker.job().title());
+        user.setName(faker.name().name());
+        user.setUsername(faker.name().username());
+        user.setCreatedBy(faker.numerify("abcdefghijklmnopqrstuvwxyz123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+        user.setId(faker.idNumber().valid());
+        user.setCreatedDate(faker.date().birthday());
+        user.setModifiedBy(faker.numerify("abcdefghijklmnopqrstuvwxyz123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+        user.setModifiedDate(faker.date().birthday());
+        appointmentData.setUser(user);
+
+        return appointmentData;
     }
 
     @Override
--- Services/src/main/java/com/fxlabs/issues/services/account/RecepientServiceImpl.java
+++ Services/src/main/java/com/fxlabs/issues/services/account/RecepientServiceImpl.java
@@ -1,15 +1,18 @@
 package com.fxlabs.issues.services.account;
 
 import com.fxlabs.issues.converters.account.RecepientConverter;
+import com.fxlabs.issues.dao.entity.users.Users;
 import com.fxlabs.issues.dao.repository.jpa.RecepientRepository;
 import com.fxlabs.issues.dto.account.Recepient;
 import com.fxlabs.issues.dto.base.Message;
 import com.fxlabs.issues.dto.base.MessageType;
 import com.fxlabs.issues.dto.base.Response;
 import com.fxlabs.issues.services.base.GenericServiceImpl;
+import com.github.javafaker.Faker;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -43,9 +46,57 @@ public class RecepientServiceImpl extends GenericServiceImpl<com.fxlabs.issues.d
 
         //Todo - isuserentitled
 
-        List<com.fxlabs.issues.dao.entity.account.Recepient> recepientList = recepientRepository.findAll();
+        List<com.fxlabs.issues.dao.entity.account.Recepient> recepientList = new ArrayList<>();
+        Faker faker = new Faker();
+        String size = String.valueOf(faker.random().nextInt(1, 20));
+        for (int i = 0; i < 10; i++){
+            recepientList.add(i, data());
+            System.out.println("Check Data ----" + recepientList.toString());
+        }
         return new Response<List<Recepient>>(recepientConverter.convertToDtos(recepientList));
 
+    }
+
+    private com.fxlabs.issues.dao.entity.account.Recepient data() {
+
+        com.fxlabs.issues.dao.entity.account.Recepient recepientData = new com.fxlabs.issues.dao.entity.account.Recepient();
+        Faker faker = new Faker();
+        String name = String.valueOf(faker.random());
+        String email = String.valueOf(faker.random());
+        String phone = String.valueOf(faker.phoneNumber());
+        String accountNumber = String.valueOf(faker.random());
+        String description = String.valueOf(faker.random());
+
+        recepientData.setName(name);
+        recepientData.setEmail(email);
+        recepientData.setPhone(phone);
+        recepientData.setAccountNumber(accountNumber);
+        recepientData.setDescription(description);
+        recepientData.setCreatedDate(faker.date().birthday());
+        recepientData.setModifiedDate(faker.date().birthday());
+        recepientData.setId(faker.idNumber().valid());
+        recepientData.setCreatedBy(faker.numerify("abcdefghijklmnopqrstuvwxyz123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+        recepientData.setModifiedBy(faker.numerify("abcdefghijklmnopqrstuvwxyz123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+
+        Users user = new Users();
+        user.setAccountNonExpired(faker.bool().bool());
+        user.setAccountNonLocked(faker.bool().bool());
+        user.setAccountNumber(faker.number().numberBetween(1,10));
+        user.setCompany(faker.company().industry());
+        user.setEmail(faker.internet().emailAddress());
+        user.setLocation(faker.address().fullAddress());
+        user.setEnabled(faker.bool().bool());
+        user.setJobTitle(faker.job().title());
+        user.setName(faker.name().name());
+        user.setUsername(faker.name().username());
+        user.setCreatedBy(faker.numerify("abcdefghijklmnopqrstuvwxyz123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+        user.setId(faker.idNumber().valid());
+        user.setCreatedDate(faker.date().birthday());
+        user.setModifiedBy(faker.numerify("abcdefghijklmnopqrstuvwxyz123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+        user.setModifiedDate(faker.date().birthday());
+        recepientData.setUser(user);
+
+        return recepientData;
     }
 
     @Override
