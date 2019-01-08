03b98a6cf5f5b6c3cbe5d094b0dd5babf7efd7d1
Zahed <sarasheed54@gmail.com>
2019-01-08 19:18:21 +0530

    Update SavingTransaction CRUD Operation

---
+++ DAO/src/main/resources/db/migration/V11__rename_column.sql
@@ -0,0 +1,7 @@
+-- Author: Zahed
+--
+-- Table: savings_transaction
+-- Column: user_id
+--
+
+alter table savings_transaction rename column user_id to users_id
--- Services/src/main/java/com/fxlabs/issues/services/transaction/SavingsTransactionServiceImpl.java
+++ Services/src/main/java/com/fxlabs/issues/services/transaction/SavingsTransactionServiceImpl.java
@@ -41,59 +41,60 @@ public class SavingsTransactionServiceImpl extends GenericServiceImpl<com.fxlabs
 
     @Override
     public Response<List<SavingsTransaction>> findAllSavingsTransaction(Integer pageSize,String currentAuditor) {
-        List<com.fxlabs.issues.dao.entity.transaction.SavingsTransaction> savingsTransactionList = new ArrayList<>(); //= savingsTransactionRepository.findAll();
-        Faker faker = new Faker();
-        int size;
-        if (pageSize > 999) {
-            size = faker.random().nextInt(1000, 1500);
-        } else {
-            size = pageSize;
-        }
-
-        for (int i = 0; i < size; i++) {
-            savingsTransactionList.set(i, data());
-        }
+      //  List<com.fxlabs.issues.dao.entity.transaction.SavingsTransaction> savingsTransactionList = new ArrayList<>(); //= savingsTransactionRepository.findAll();
+        List<com.fxlabs.issues.dao.entity.transaction.SavingsTransaction> savingsTransactionList = savingsTransactionRepository.findAll();
+//        Faker faker = new Faker();
+//        int size;
+//        if (pageSize > 999) {
+//            size = faker.random().nextInt(1000, 1500);
+//        } else {
+//            size = pageSize;
+//        }
+//
+//        for (int i = 0; i < size; i++) {
+//            savingsTransactionList.set(i, data());
+//        }
 
         return new Response<List<SavingsTransaction>>(converter.convertToDtos(savingsTransactionList));
      }
 
-    public com.fxlabs.issues.dao.entity.transaction.SavingsTransaction data() {
-        com.fxlabs.issues.dao.entity.transaction.SavingsTransaction savingsTransaction = new com.fxlabs.issues.dao.entity.transaction.SavingsTransaction();
-        Faker faker = new Faker();
-        String description = String.valueOf(faker.random());
-        String type = String.valueOf(faker.random());
-        String status = String.valueOf(faker.random());
-        double amount = (Double) Double.valueOf(String.valueOf(faker.number().digits(6)));
-        BigDecimal availableBalance = BigDecimal.valueOf((Double) Double.valueOf(String.valueOf(faker.number().numberBetween(0,7))));
-
-        savingsTransaction.setAmount(amount);
-        savingsTransaction.setAvailableBalance(availableBalance);
-        savingsTransaction.setDescription(description);
-        savingsTransaction.setStatus(status);
-        savingsTransaction.setType(type);
-        savingsTransaction.setCreatedDate(faker.date().birthday());
-        savingsTransaction.setModifiedDate(faker.date().birthday());
-        savingsTransaction.setId(faker.idNumber().valid());
-        savingsTransaction.setCreatedBy(faker.numerify("abcdefghijklmnopqrstuvwxyz123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
-        savingsTransaction.setModifiedBy(faker.numerify("abcdefghijklmnopqrstuvwxyz123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
-
-        Users user = new Users();
-        user.setAccountNonExpired(faker.bool().bool());
-        user.setAccountNonLocked(faker.bool().bool());
-        user.setAccountNumber(faker.number().numberBetween(1,10));
-        user.setCompany(faker.company().industry());
-        user.setEmail(faker.internet().emailAddress());
-        user.setLocation(faker.address().fullAddress());
-        user.setEnabled(faker.bool().bool());
-        user.setJobTitle(faker.job().title());
-        user.setName(faker.name().name());
-        user.setUsername(faker.name().username());
-        user.setCreatedBy(faker.numerify("abcdefghijklmnopqrstuvwxyz123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
-        user.setId(faker.idNumber().valid());
-        user.setCreatedDate(faker.date().birthday());
-        user.setModifiedBy(faker.numerify("abcdefghijklmnopqrstuvwxyz123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
-        user.setModifiedDate(faker.date().birthday());
-        savingsTransaction.setUser(user);
-        return savingsTransaction;
-    }
+//    public com.fxlabs.issues.dao.entity.transaction.SavingsTransaction data() {
+//        com.fxlabs.issues.dao.entity.transaction.SavingsTransaction savingsTransaction = new com.fxlabs.issues.dao.entity.transaction.SavingsTransaction();
+//        Faker faker = new Faker();
+//        String description = String.valueOf(faker.random());
+//        String type = String.valueOf(faker.random());
+//        String status = String.valueOf(faker.random());
+//        double amount = (Double) Double.valueOf(String.valueOf(faker.number().digits(6)));
+//        BigDecimal availableBalance = BigDecimal.valueOf((Double) Double.valueOf(String.valueOf(faker.number().numberBetween(0,7))));
+//
+//        savingsTransaction.setAmount(amount);
+//        savingsTransaction.setAvailableBalance(availableBalance);
+//        savingsTransaction.setDescription(description);
+//        savingsTransaction.setStatus(status);
+//        savingsTransaction.setType(type);
+//        savingsTransaction.setCreatedDate(faker.date().birthday());
+//        savingsTransaction.setModifiedDate(faker.date().birthday());
+//        savingsTransaction.setId(faker.idNumber().valid());
+//        savingsTransaction.setCreatedBy(faker.numerify("abcdefghijklmnopqrstuvwxyz123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+//        savingsTransaction.setModifiedBy(faker.numerify("abcdefghijklmnopqrstuvwxyz123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+//
+//        Users user = new Users();
+//        user.setAccountNonExpired(faker.bool().bool());
+//        user.setAccountNonLocked(faker.bool().bool());
+//        user.setAccountNumber(faker.number().numberBetween(1,10));
+//        user.setCompany(faker.company().industry());
+//        user.setEmail(faker.internet().emailAddress());
+//        user.setLocation(faker.address().fullAddress());
+//        user.setEnabled(faker.bool().bool());
+//        user.setJobTitle(faker.job().title());
+//        user.setName(faker.name().name());
+//        user.setUsername(faker.name().username());
+//        user.setCreatedBy(faker.numerify("abcdefghijklmnopqrstuvwxyz123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+//        user.setId(faker.idNumber().valid());
+//        user.setCreatedDate(faker.date().birthday());
+//        user.setModifiedBy(faker.numerify("abcdefghijklmnopqrstuvwxyz123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+//        user.setModifiedDate(faker.date().birthday());
+//        savingsTransaction.setUser(user);
+//        return savingsTransaction;
+//    }
 }
